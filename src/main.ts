f8388684ff9c8f280dff1fca6e9b8863e02bd552
HORDEQUIN Alexandre (SNCF RESEAU / EIC LANGUEDOC ROUSSILLON / NARBONNE) <7809188y@commun.ad.sncf.fr>
2023-02-06 13:52:32 +0100

    merge

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('findadev')
+    .setDescription('Findadev API description')
+    .setVersion('1.0')
+    .addTag('dev')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({
     // retire tout les champs qui ne sont pas déclaré dans la dto
     whitelist: true,
     // rejette les requêtes qui contiennent des champs non déclaré dans la dto
-    forbidNonWhitelisted: true, 
+    forbidNonWhitelisted: true,
 
     transform: true
     //permet la transformation format date
